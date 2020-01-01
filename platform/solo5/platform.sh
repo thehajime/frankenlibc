a751fcfd4e335167e4cdbd8479de8f593ecc3944
Akira Moroo <retrage01@gmail.com>
2020-01-01 20:29:05 +0900

    add solo5 spt linker script
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- platform/solo5/platform.sh
+++ platform/solo5/platform.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-LINKSCRIPT="${PWD}/solo5/bindings/spt/solo5_spt.lds"
+LINKSCRIPT="${PWD}/platform/solo5/solo5_spt.lds"
 EXTRA_LDSCRIPT="-T ${LINKSCRIPT}"
 EXTRA_LDSCRIPT_CC="-Wl,-T,${LINKSCRIPT}"
