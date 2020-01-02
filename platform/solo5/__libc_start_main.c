8fba4a837d2b45af72974242418a6a5f198186c9
Akira Moroo <retrage01@gmail.com>
2020-01-02 16:26:02 +0900

    fix envp on solo5 platform
    This commits fixes SEGV in tests/hello-pthread. It also removes
    RUMP_VERBOSE environment variable passing.
    
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- platform/solo5/__libc_start_main.c
+++ platform/solo5/__libc_start_main.c
@@ -9,12 +9,15 @@ extern int main(int, char **, char **);
 int __franken_start_main(int (*)(int, char **, char **), int, char **, char **);
 int argc = 1;
 char *argv[2] = {"solo5", "\0"};
-char *envp[2] = {"RUMP_VERBOSE=1", "\0"};
 
 int solo5_app_main(const struct solo5_start_info *info)
 {
+	char **envp;
+
 	solo5_heap_start = info->heap_start;
     solo5_heap_end = info->heap_start + info->heap_size;
+
+    envp = argv + argc + 1;
 	/* TODO: parse solo5_start_info.cmdline */
 	return __franken_start_main(main, argc, argv, envp);
 }
