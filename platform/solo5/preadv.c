8686c8983383d8558e9b336904a7c3ef705d45da
Akira Moroo <retrage01@gmail.com>
2020-01-10 02:23:45 +0900

    cleanup preadv/pwritev for solo5 platform
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- platform/solo5/preadv.c
+++ platform/solo5/preadv.c
@@ -12,26 +12,28 @@ ssize_t
 preadv(int fd, const struct iovec *iov, int iovcnt, off_t off)
 {
   int i, j;
-  off_t offset;
   ssize_t count;
+  size_t block_size;
   solo5_result_t res;
 
   if (fd != SOLO5_ROOTFS_FD || !solo5_rootfs_handle)
     return EINVAL;
 
-  offset = off;
-  count = 0;
+  block_size = solo5_rootfs_info->block_size;
+  if (off & (block_size - 1))
+    return EINVAL;
 
+  count = 0;
   for (i = 0; i < iovcnt; i++) {
-    for (j = 0; j * solo5_rootfs_info->block_size < iov[i].iov_len; j++) {
+    if (iov[i].iov_len & (block_size - 1))
+      return EINVAL;
+    for (j = 0; j * block_size < iov[i].iov_len; j++) {
       res = solo5_block_read(solo5_rootfs_handle, 
-          (solo5_off_t)(offset + j * solo5_rootfs_info->block_size),
-          iov[i].iov_base + j * solo5_rootfs_info->block_size,
-          solo5_rootfs_info->block_size);
+          (solo5_off_t)(off + count + j * block_size),
+          iov[i].iov_base + j * block_size, block_size);
       if (res != SOLO5_R_OK)
         return EIO;
     }
-    offset += iov[i].iov_len;
     count += iov[i].iov_len;
   }
 
