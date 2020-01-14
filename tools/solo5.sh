b9b2d8789947d048ec481a2dc312baaf5b68236d
Akira Moroo <retrage01@gmail.com>
2020-01-14 23:57:11 +0900

    improve rexec bash script wrapper
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- tools/solo5.sh
+++ tools/solo5.sh
@@ -1,3 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
-solo5-spt "$@"
+ROOTFS=""
+KERNEL=""
+ENVIRON=""
+ARGS=""
+
+for arg in $(env); do
+  ENVIRON="${ENVIRON} \"${arg}\""
+done
+
+ARGS_FLAG=""
+for arg in "$@"; do
+  case ${arg} in
+    rootfs:*)
+      ROOTFS="$(echo ${arg} | cut -d ":" -f2)"
+      ;;
+    --|*)
+      [ "${arg}" == "--" ] && ARGS_FLAG="1"
+      [ -n "${ARGS_FLAG}" ] && ARGS="${ARGS} \"${arg}\""
+      [ -z "${ARGS_FLAG}" ] && KERNEL="${arg}"
+      ;;
+  esac
+done
+
+[ -z "${KERNEL}" ] && printf "solo5 kernel is not specified\n" && exit -1
+[ -z "${ROOTFS}" ] && printf "rootfs is not specified\n" && exit -1
+
+solo5/tenders/spt/solo5-spt \
+  --block:rootfs=${ROOTFS} \
+  ${KERNEL} \
+  ${ENVIRON} \
+  ${ARGS}
